661aab1b2b278094d0fa93ee984b00cdbda9e97d
ogimgio <ogimgamers@gmail.com>
2021-12-01 11:00:43 +0100

    init

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+import VueApexCharts from 'vue-apexcharts'
+Vue.use(VueApexCharts)
+
+Vue.component('apexchart', VueApexCharts)
 
 new Vue({
   render: h => h(App),
